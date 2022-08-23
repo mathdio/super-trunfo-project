d4152d6067fd4a6266ba928d76e2e65875e59e00
matheus <mathdiogenes@gmail.com>
2022-08-23 16:10:44 -0300

    Req 4 em andamento

--- src/App.js
+++ src/App.js
@@ -3,19 +3,34 @@ import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends React.Component {
+  state = {
+    name: '',
+    description: '',
+    attr1: '',
+    attr2: '',
+    attr3: '',
+    image: '',
+    rarity: '',
+    isSuperTrunfo: false,
+  };
+
   render() {
+    const {
+      name, description, attr1, attr2, attr3, image,
+      rarity, isSuperTrunfo,
+    } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
         <Form
-          cardName=""
-          cardDescription=""
-          cardAttr1=""
-          cardAttr2=""
-          cardAttr3=""
-          cardImage=""
-          cardRare=""
-          cardTrunfo={ false }
+          cardName={ name }
+          cardDescription={ description }
+          cardAttr1={ attr1 }
+          cardAttr2={ attr2 }
+          cardAttr3={ attr3 }
+          cardImage={ image }
+          cardRare={ rarity }
+          cardTrunfo={ isSuperTrunfo }
           hasTrunfo={ false }
           isSaveButtonDisabled={ false }
           onInputChange={ () => {} }
